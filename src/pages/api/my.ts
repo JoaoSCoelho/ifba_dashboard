31cc847a737c0258b865d9e786ffea0a7cb96e3d
João Victor <joaovictor-san-co@hotmail.com>
2021-03-24 10:43:20 -0300

    create route /api/my

---
+++ src/pages/api/my.ts
@@ -0,0 +1,66 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { firestore } from "../../firebase";
+import jwt from "jsonwebtoken";
+
+let rateLimitAccumulator: {
+  timestamp: number;
+  ip: string;
+  times: number;
+}[] = [];
+
+export default async (req: IncomingMessage, res: ServerResponse) => {
+  // @ts-ignore
+  if (req.method !== "GET") return res.status(404).send("Not found!");
+
+  rateLimitAccumulator = rateLimitAccumulator.filter(
+    (x) => Date.now() - x.timestamp <= 1000 * 60
+  );
+  if (
+    rateLimitAccumulator.find(
+      (x) =>
+        x.ip === req.socket.remoteAddress &&
+        x.times > 5 &&
+        Date.now() - x.timestamp < 1000 * 60
+    )
+  )
+    return res.status(429).send("To many requests");
+  else if (
+    rateLimitAccumulator.find((x) => x.ip === req.socket.remoteAddress)
+  ) {
+    rateLimitAccumulator.find((x) => x.ip === req.socket.remoteAddress).times++;
+  } else {
+    rateLimitAccumulator.push({
+      ip: req.socket.remoteAddress,
+      times: 1,
+      timestamp: Date.now(),
+    });
+  }
+
+  if (
+    !req.headers?.token ||
+    typeof req.headers.token !== "string" ||
+    !/Bearer .+/g.test(req.headers.token)
+  )
+    return res.status(400).send("Bad request!");
+
+  let decoded;
+
+  try {
+    decoded = jwt.verify(req.headers.token.slice(7), process.env.SECRET);
+  } catch (error) {
+    return res.status(400).send("Bad request!");
+  }
+
+  const accounts = firestore.collection("accounts");
+  const filteredAccounts = await accounts.doc(String(decoded.account)).get();
+  const account = filteredAccounts.data();
+
+  if (!account) return res.status(400).send("Bad request!");
+
+  account.acessKey = undefined;
+  account.createdAt = undefined;
+
+  return res.status(200).json({
+    account,
+  });
+};
